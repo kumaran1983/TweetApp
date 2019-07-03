0bbbeea97e18fdbf696f95009e553d311d6219a7
mumani <mumani@ebay.com>
2019-07-02 19:57:13 -0700

    Adding json annotation

--- src/main/java/com/salesforce/tweetapi/resource/TweetController.java
+++ src/main/java/com/salesforce/tweetapi/resource/TweetController.java
@@ -3,6 +3,8 @@ package com.salesforce.tweetapi.resource;
 import com.salesforce.tweetapi.entity.HashTag;
 import com.salesforce.tweetapi.resource.entity.*;
 import com.salesforce.tweetapi.service.ITweetService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -14,21 +16,24 @@ import java.util.List;
 @RestController
 public class TweetController {
 
+    public static Logger log = LoggerFactory.getLogger(TweetController.class);
+
     @Autowired
     ITweetService tweetService;
 
-    //The max number of results which can requested from application.properties
-    @Value( "${limit.max}" )
-    private int maxLimit;
+    @Value("${limit.default}")
+    private int defaultLimit;
 
     @RequestMapping(value = "/tweets", method = RequestMethod.POST)
-    public ResponseEntity<HashTagResponse> topHashTags(@RequestBody List<Tweet> tweets, @RequestParam(value = "limit", defaultValue = "10") final Integer limit) {
+    public ResponseEntity<HashTagResponse> topHashTags(@RequestBody List<Tweet> tweets, @RequestParam(value = "limit", required=false) Integer limit) {
 
         HashTagResponse hashTagResponse = new HashTagResponse();
 
         //Perform validation on limit (Max number of results requested by user)
         //Have validation classes if there are lot of input validations needed.
-        if(limit > maxLimit || limit < 1) {
+        if(limit == null) {
+            limit = defaultLimit;
+        }else if(limit > defaultLimit || limit < 1) {
             hashTagResponse.setMessage(new Message(MessageType.ERROR, 1001, "INVALID LIMIT", MessageCategory.APP));
             return new ResponseEntity<>(hashTagResponse,HttpStatus.BAD_REQUEST);
         }
@@ -38,6 +43,7 @@ public class TweetController {
             hashTagResponse.setTopTags(hashTags);
             return new ResponseEntity<>(hashTagResponse, HttpStatus.OK);
         } catch (Exception ex) {
+            log.error("TweetController Error : ", ex);
             hashTagResponse.setMessage(new Message(MessageType.ERROR, 1000, "SYSTEM ERROR", MessageCategory.SYSTEM));
             return new ResponseEntity<>(hashTagResponse,HttpStatus.INTERNAL_SERVER_ERROR);
         }
